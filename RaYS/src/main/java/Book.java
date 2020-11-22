17f320b450be83ba833f6c8137983f21b97fc98f
Chris <christoph.frischmuth@fh-erfurt.de>
2020-11-22 14:07:12 +0100

    Neu: Medium als abstrakte Klasse und der erste Teil von Book

---
+++ RaYS/src/main/java/Book.java
@@ -0,0 +1,10 @@
+public class Book extends Medium //extends = erbt
+{
+    private String publisher;
+    private Person author;
+    private String ISBN;
+    private boolean esEBook;
+    private boolean isPrint;
+    private String languages;
+    private Collection bookSeries;
+}
---
+++ RaYS/src/main/java/Medium.java
@@ -0,0 +1,80 @@
+import java.util.Date;
+
+abstract class Medium
+{
+    private String name;
+    private Date publicationDate;
+    private Comment[] comment; //Class will be implemented
+    private String shortDescription;
+    private float averageRating;
+    private String personalOpinion;
+    private int ratingCounter;
+    private Collection[] collections; //Class will be implemented
+
+
+    //Set-/Get-Section
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setComment(Comment[] comment) {
+        this.comment = comment;
+    }
+
+    public Comment[] getComment() {
+        return comment;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setAverageRating(float averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public float getAverageRating() {
+        return averageRating;
+    }
+
+    public void setPersonalOpinion(String personalOpinion) {
+        this.personalOpinion = personalOpinion;
+    }
+
+    public String getPersonalOpinion() {
+        return personalOpinion;
+    }
+
+    public void setRatingCounter(int ratingCounter) {
+        this.ratingCounter = ratingCounter;
+    }
+
+    public int getRatingCounter() {
+        return ratingCounter;
+    }
+
+    public void setCollections(Collection[] collections) {
+        this.collections = collections;
+    }
+
+    public Collection[] getCollections() {
+        return collections;
+    }
+
+}
