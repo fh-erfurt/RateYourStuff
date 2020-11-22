7f46363beb80b9eb6396a56cf74babc318d3bd2d
Chris <christoph.frischmuth@fh-erfurt.de>
2020-11-22 14:29:21 +0100

    V1 of Book completed

--- RaYS/src/main/java/Book.java
+++ RaYS/src/main/java/Book.java
@@ -1,10 +1,81 @@
 public class Book extends Medium //extends = erbt
 {
-    private String publisher;
-    private Person author;
+    private String publisher[];
+    private Person author[];
     private String ISBN;
-    private boolean esEBook;
+    private boolean isEBook;
     private boolean isPrint;
-    private String languages;
-    private Collection bookSeries;
+    private String languages[];
+    private Collection bookSeries[];
+    private int numberOfPages;
+
+    //Set-/ Get-Section
+
+    public void setPublisher(String[] publisher) {
+        this.publisher = publisher;
+    }
+
+    public String[] getPublisher() {
+        return publisher;
+    }
+
+    public void setAuthor(Person[] author) {
+        this.author = author;
+    }
+
+    public Person[] getAuthor() {
+        return author;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setIsEBook(boolean isEBook) {
+        this.isEBook = isEBook;
+    }
+
+    public boolean getIsEbook()
+    {
+        return isEBook;
+    }
+
+    public void setIsPrint(boolean isPrint)
+    {
+        this.isPrint = isPrint;
+    }
+
+    public boolean getIsPrint()
+    {
+        return isPrint;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setBookSeries(Collection[] bookSeries) {
+        this.bookSeries = bookSeries;
+    }
+
+    public Collection[] getBookSeries() {
+        return bookSeries;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
 }
