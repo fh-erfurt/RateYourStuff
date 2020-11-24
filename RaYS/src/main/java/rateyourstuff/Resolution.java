3d7aa871f60ceeee0fca30cc56e3deb270102dd9
Chris <christoph.frischmuth@fh-erfurt.de>
2020-11-24 20:10:40 +0100

    Prototype class Resolution for better express of Resolution in classes Series, Game and Movie

---
+++ RaYS/src/main/java/rateyourstuff/Resolution.java
@@ -0,0 +1,23 @@
+package rateyourstuff;
+
+public class Resolution {
+    private int width;
+    private int height;
+
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
