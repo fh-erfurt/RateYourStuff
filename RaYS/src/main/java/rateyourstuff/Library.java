ba4e58b07923c8ebc18d0a5042a3f57b3d4f81a8
MKnop2 <kickmepony@gmail.com>
2020-11-26 09:00:27 +0100

    Adder in class Library

--- RaYS/src/main/java/rateyourstuff/Library.java
+++ RaYS/src/main/java/rateyourstuff/Library.java
@@ -27,12 +27,6 @@ public class Library
     // Getter / Setter / Adder
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public List<Medium> getMedia() {
-        return media;
-    }
-
-    public void setMedia(List<Medium> media) {
-        this.media = media;
-    }
-
-}
+    public List<Medium> getMedia() { return media; }
+    public void setMedia(List<Medium> media) { this.media = media; }
+    public void AddMedia(List<Medium> media) { this.media.addAll(media); }}
